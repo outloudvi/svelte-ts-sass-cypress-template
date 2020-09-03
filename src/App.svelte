71647896914cfbadff97422c76f8df40bd5b2531
Outvi V <git@outv.im>
2020-09-03 12:37:26 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<main>
+  <p>Welcome to {project_name}!</p>
+  <Hello />
+</main>
+
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import Hello from './components/Hello.svelte'
+
+  export let project_name: string
+
+  onMount(() => {
+    console.log('Hello, Svelte')
+  })
+</script>
+
+<style lang="scss">
+  p {
+    text-align: center;
+  }
+</style>
---
+++ src/components/Hello.svelte
@@ -0,0 +1,19 @@
+<div>{$_('Hello, world!')}</div>
+
+<script lang="ts">
+  import { _ } from 'svelte-i18n'
+</script>
+
+<style lang="scss">
+  @import '../styles/variables';
+
+  div {
+    text-align: center;
+    color: $hello-color;
+  }
+
+  // To modify something outside the scope, use :global()
+  :global(p) {
+    font-size: 2rem;
+  }
+</style>
